6e08e8903d6599ad4599088fb209c39080c33f00
zhang qing <zhangqing332@live.com>
2017-01-15 21:22:05 +0800

    init project

---
+++ content/content.js
@@ -0,0 +1,15 @@
+// Listen for messages
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+	// If the received message has the expected format...
+	if (msg.text === 'report_back') {
+		// Call the specified callback, passing
+		// the web-page's DOM content as argument
+		sendResponse(document.all[0].outerHTML);
+	}
+});
+var allInputs = document.body.querySelectorAll('input[type=text]'),
+	randomText = Math.random();
+for(var i=0; i<allInputs.length ; i++){
+	allInputs[i].value = i+": "+ randomText;
+}
+console.log(allInputs.length);
